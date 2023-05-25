53c46603c729d7d9914cfdd0f0f1efd89192c661
Enyo <enyoonuche1@gmail.com>
2023-05-25 23:28:34 +0100

    Updated Code

--- src/Card.jsx
+++ src/Card.jsx
@@ -31,7 +31,7 @@ const Card = () => {
                 </div>
                 <div className="flex relative">
                     <img width={30} height={30} src={ms} alt="" className="absolute left-2 top-[14px]"/>
-                    <input type="tel" pattern="\d*" placeholder="2142 - 7512 - 3412 - 3456" inputmode="numeric" maxLength={16} className="focus:border-blue focus:text-blue focus:border-2 outline-none w-full border border-slate-200 py-4 px-16  rounded-lg" />
+                    <input type="tel" pattern="\d*" placeholder="2142 - 7512 - 3412 - 3456" inputmode="numeric" maxLength={16} className="focus:border-blue text-primary focus:border-2 outline-none w-full border border-slate-200 py-4 px-16  rounded-lg" />
                     <img width={30} height={30} src={tick} className="absolute right-2 top-[14px]" alt="" />
                 </div>
                 
@@ -39,24 +39,24 @@ const Card = () => {
             <Input title="CVV Number" subtitle="Enter the 3 or 4 digit number on the card">
                 <div className="relative">
                     <img width={30} height={30} src={grid} alt="" className="absolute right-2 top-[14px]"/> 
-                    <input type="text" maxLength={4} placeholder="327" className="focus:border-blue focus:text-blue focus:border-2 outline-none w-full border border-slate-200 p-4 pr-16 rounded-lg" />
+                    <input type="text" maxLength={4} placeholder="327" className="focus:border-blue text-primary focus:border-2 outline-none w-full border border-slate-200 p-4 pr-16 rounded-lg" />
                 </div>
                 
             </Input>
             <Input title='Expiry Date' subtitle='Enter expiration date of the card'>
                 <div className="flex gap-2 items-center">
-                    <input placeholder="9" max={31} type="number" className="focus:border-blue focus:text-blue focus:border-2 outline-none w-full border border-slate-200 p-4 rounded-lg" />
+                    <input placeholder="9" max={31} type="number" className="focus:border-blue  text-primary focus:border-2 outline-none w-full border border-slate-200 p-4 rounded-lg" />
                         <p className="font-bold">/</p>
-                    <input placeholder="24" maxLength={2} type="number" className="focus:border-blue focus:text-blue focus:border-2 outline-none w-full border border-slate-200 p-4 rounded-lg" />
+                    <input placeholder="24" maxLength={2} type="number" className="focus:border-blue focus:border-2 outline-none w-full border border-slate-200 p-4 rounded-lg" />
                 </div>
             </Input>
             <Input title='Password' subtitle='Enter your Dynamic password'>
                 <div className="relative">
                         <img width={30} height={30} src={grid} alt="" className="absolute right-2 top-[14px]"/> 
-                        <input type="password" placeholder="············" className="focus:border-blue focus:text-blue focus:border-2 outline-none w-full border border-slate-200 p-4 pr-16 rounded-lg" />
+                        <input type="password" placeholder="············" className="focus:border-blue text-primary focus:text-blue focus:border-2 outline-none w-full border border-slate-200 p-4 pr-16 rounded-lg" />
                 </div>
             </Input>
-            <button className="focus:border-blue focus:text-blue focus:border-2 outline-none bg-blue p-4 w-full font-medium rounded-lg text-white">Pay now</button>
+            <button className="focus:border-blue  focus:border-2 outline-none bg-blue p-4 w-full font-medium rounded-lg text-white">Pay now</button>
         </div>
         <CardSlip />
     </div> );
